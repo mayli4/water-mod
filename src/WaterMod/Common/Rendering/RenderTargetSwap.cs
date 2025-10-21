4abb9c817c3f74829ce90c75286abce9711e0c41
mayli4 <sydsleeper@gmail.com>
2025-10-21 02:47:10 -0400

    feat: rt swap

---
+++ src/WaterMod/Common/Rendering/RenderTargetSwap.cs
@@ -0,0 +1,113 @@
+﻿namespace WaterMod.Common.Rendering;
+
+//https://github.com/ZenTheMod/ZensSky/blob/main/Core/DataStructures/RenderTargetSwap.cs
+internal readonly ref struct RenderTargetSwap {
+    #region Private Properties
+
+    private RenderTargetBinding[] OldTargets { get; init; }
+
+    private Rectangle OldScissor { get; init; }
+
+    #endregion
+
+    #region Public Constructors
+
+    public RenderTargetSwap(RenderTarget2D? target)
+    {
+        GraphicsDevice device = Main.instance.GraphicsDevice;
+
+        OldTargets = device.GetRenderTargets();
+        OldScissor = device.ScissorRectangle;
+
+            // Set the default RenderTargetUsage to PreserveContents to prevent clearing the prior targets when swapping back in Dispose().
+        foreach (RenderTargetBinding oldTarget in OldTargets)
+            if (oldTarget.RenderTarget is RenderTarget2D rt)
+                rt.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+
+        device.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+
+        device.SetRenderTarget(target);
+        device.ScissorRectangle = new(0, 0,
+            target?.Width ?? Main.graphics.PreferredBackBufferWidth,
+            target?.Height ?? Main.graphics.PreferredBackBufferHeight);
+    }
+
+    public RenderTargetSwap(
+        ref RenderTarget2D? target,
+        int width,
+        int height,
+        bool mipMap = false,
+        SurfaceFormat preferredFormat = SurfaceFormat.Color,
+        DepthFormat preferredDepthFormat = DepthFormat.None,
+        int preferredMultiSampleCount = 0,
+        RenderTargetUsage usage = RenderTargetUsage.PreserveContents)
+    {
+        GraphicsDevice device = Main.instance.GraphicsDevice;
+
+        OldTargets = device.GetRenderTargets();
+        OldScissor = device.ScissorRectangle;
+
+        ReintializeTarget(
+            ref target,
+            device,
+            width,
+            height,
+            mipMap,
+            preferredFormat,
+            preferredDepthFormat,
+            preferredMultiSampleCount,
+            usage);
+
+        foreach (RenderTargetBinding oldTarget in OldTargets)
+            if (oldTarget.RenderTarget is RenderTarget2D rt)
+                rt.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+
+        device.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+
+        device.SetRenderTarget(target);
+        device.ScissorRectangle = new(0, 0,
+            target?.Width ?? Main.graphics.PreferredBackBufferWidth,
+            target?.Height ?? Main.graphics.PreferredBackBufferHeight);
+    }
+
+    #endregion
+    
+    public static void ReintializeTarget(ref RenderTarget2D? target, 
+        GraphicsDevice device,
+        int width,
+        int height,
+        bool mipMap = false,
+        SurfaceFormat preferredFormat = SurfaceFormat.Color,
+        DepthFormat preferredDepthFormat = DepthFormat.None,
+        int preferredMultiSampleCount = 0,
+        RenderTargetUsage usage = RenderTargetUsage.PreserveContents)
+    {
+        if (target is null ||
+            target.IsDisposed ||
+            target.Width != width ||
+            target.Height != height)
+        {
+            target?.Dispose();
+            target = new(device,
+                width,
+                height,
+                mipMap,
+                preferredFormat,
+                preferredDepthFormat,
+                preferredMultiSampleCount,
+                usage);
+        }
+    }
+
+    #region Disposable Pattern
+
+    public void Dispose()
+    {
+        GraphicsDevice device = Main.instance.GraphicsDevice;
+
+        device.SetRenderTargets(OldTargets);
+        device.ScissorRectangle = OldScissor;
+    }
+
+    #endregion
+}
