7fe53a002f666c33ac0b9e87528a37200bc075a8
mayli4 <sydsleeper@gmail.com>
2025-11-02 16:47:02 -0500

    feat: special tile drawing system

---
+++ src/WaterMod/Common/Rendering/SpecialTileDrawing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Terraria.GameContent.Drawing;
+
+namespace WaterMod.Common.Rendering;
+
+public interface ITileSpecialDrawn {
+    void SpecialDraw(Vector2 screenPosition, Point pos, SpriteBatch spriteBatch, TileDrawing tileDrawing);
+}
+
+internal record struct SpecialTile(ModTile Tile, Point Position);
+
+internal sealed class TileSpecialDrawManager : ModSystem {
+    internal static List<SpecialTile> Tiles = [];
+
+    public static void AddSpecialPoint(ModTile modTile, int i, int j) {
+        if(Tiles.Contains(new SpecialTile(modTile, new Point(i, j))) || modTile is not ITileSpecialDrawn)
+            return;
+
+        Tiles.Add(new SpecialTile(modTile, new Point(i, j)));
+    }
+
+    public override void Load() {
+        On_TileDrawing.PreDrawTiles += (orig, self, solidLayer, forRenderTargets, intoRenderTargets) =>
+        {
+            orig.Invoke(self, solidLayer, forRenderTargets, intoRenderTargets);
+            bool flag = intoRenderTargets || Lighting.UpdateEveryFrame;
+            if(!solidLayer && flag) {
+                Tiles.Clear();
+            }
+        };
+
+        On_TileDrawing.DrawReverseVines += (orig, self) =>
+        {
+            orig.Invoke(self);
+
+            Vector2 unscaledPosition = Main.Camera.UnscaledPosition;
+            foreach((ModTile modTile, Point position) in Tiles) {
+                if(modTile is ITileSpecialDrawn tileFluent) {
+                    tileFluent.SpecialDraw(unscaledPosition, position, Main.spriteBatch, self);
+                }
+            }
+        };
+    }
+}
