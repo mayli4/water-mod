670f8a9856fc679b160666e225606a16caf5ec4f
mayli4 <sydsleeper@gmail.com>
2025-11-01 23:36:48 -0400

    feat: basic seamap ship sync

--- src/WaterMod/Content/Seamap/SeamapPlayer.cs
+++ src/WaterMod/Content/Seamap/SeamapPlayer.cs
@@ -1,19 +1,25 @@
 ﻿using Daybreak.Common.Features.Hooks;
+using JetBrains.Annotations;
 using Microsoft.Xna.Framework.Input;
 using ReLogic.Graphics;
 using SubworldLibrary;
+using System.IO;
 using Terraria.DataStructures;
 using Terraria.GameContent;
-using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
+using Terraria.ID;
+using WaterMod.Common.Networking;
 
 namespace WaterMod.Content.Seamap;
 
 internal sealed class SeamapPlayer : ModPlayer {
-    //todo make this more robust, upgrades etc
     public int ShipMaxLife { get; set; } = 100;
     public int ShipCurrentLife { get; set; } = 100;
-    
+    private int _previousShipCurrentLife;
+
+    public override void Initialize() {
+        _previousShipCurrentLife = ShipCurrentLife;
+    }
+
     public override void PreUpdate() {
         if (!SubworldSystem.IsActive<SeamapSubworld>())
             return;
@@ -30,34 +36,44 @@ internal sealed class SeamapPlayer : ModPlayer {
         ShipCurrentLife -= damage;
         if (ShipCurrentLife < 0) {
             ShipCurrentLife = 0;
-            Player.KillMe(PlayerDeathReason.ByOther(1), 1, 0);
+            if (Player.whoAmI == Main.myPlayer || Main.netMode == NetmodeID.Server) {
+                Player.KillMe(PlayerDeathReason.ByOther(1), 1, 0);
+            }
+        }
+    }
+    
+    public void HealShipDamage(int amount) {
+        ShipCurrentLife += amount;
+        if (ShipCurrentLife > ShipMaxLife) {
+            ShipCurrentLife = ShipMaxLife;
         }
     }
 
     public override void PostUpdate() {
         if(Main.keyState.IsKeyDown(Keys.O) && !Main.oldKeyState.IsKeyDown(Keys.O) ) {
-            TakeShipDamage(5);
+            if (Main.netMode != NetmodeID.MultiplayerClient || Player.whoAmI == Main.myPlayer) {
+                TakeShipDamage(5);
+            }
         }
         
-        //Main.NewText("ship life: " + ShipCurrentLife);
+        if(Main.keyState.IsKeyDown(Keys.P) && !Main.oldKeyState.IsKeyDown(Keys.P) ) {
+            if (Main.netMode != NetmodeID.MultiplayerClient || Player.whoAmI == Main.myPlayer) {
+                HealShipDamage(5);
+            }
+        }
+        
+        if (ShipCurrentLife != _previousShipCurrentLife) {
+            _previousShipCurrentLife = ShipCurrentLife;
+            
+            if (Main.netMode == NetmodeID.MultiplayerClient && Player.whoAmI == Main.myPlayer) {
+                new ShipHealthPacket(Player.whoAmI, ShipCurrentLife).Send();
+            } else if (Main.netMode == NetmodeID.Server) {
+                new ShipHealthPacket(Player.whoAmI, ShipCurrentLife).Send(ignoreClient: Player.whoAmI);
+            }
+        }
     }
-
-    public override void SendClientChanges(ModPlayer clientPlayer) {
-        base.SendClientChanges(clientPlayer);
-    }
-
-    public override void CopyClientState(ModPlayer targetCopy) {
-        base.CopyClientState(targetCopy);
-    }
-
-    public override void SaveData(TagCompound tag) {
-        base.SaveData(tag);
-    }
-
-    public override void LoadData(TagCompound tag) {
-        base.LoadData(tag);
-    }
-
+    
+    [UsedImplicitly]
     [SubscribesTo<ModSystemHooks.PostDrawTiles>]
     static void DrawLifeText(ModSystemHooks.PostDrawTiles.Original orig, ModSystem system) {
         orig();
@@ -85,5 +101,39 @@ internal sealed class SeamapPlayer : ModPlayer {
         }
         
         Main.spriteBatch.End();
+    }
+}
+
+
+internal class ShipHealthPacket() : PacketData {
+    public int PlayerId { get; private set; }
+    public int CurrentShipHealth { get; private set; }
+
+    public ShipHealthPacket(int playerId, int currentShipHealth) : this() {
+        PlayerId = playerId;
+        CurrentShipHealth = currentShipHealth;
+    }
+
+    public override void OnSend(ModPacket modPacket) {
+        modPacket.Write(PlayerId);
+        modPacket.Write(CurrentShipHealth);
+    }
+
+    public override void OnReceive(BinaryReader reader, int whoAmI) {
+        int playerId = reader.ReadInt32();
+        int currentShipHealth = reader.ReadInt32();
+
+        if (playerId is < 0 or >= Main.maxPlayers)
+            return;
+
+        var player = Main.player[playerId];
+        if (player.active) {
+            SeamapPlayer seamapPlayer = player.GetModPlayer<SeamapPlayer>();
+            seamapPlayer.ShipCurrentLife = currentShipHealth;
+        }
+
+        if (Main.netMode == NetmodeID.Server) {
+            new ShipHealthPacket(playerId, currentShipHealth).Send(ignoreClient: whoAmI);
+        }
     }
 }
