0cf4809bc08c2e264e30fd68b25ee1ce85650634
mayli4 <sydsleeper@gmail.com>
2025-11-02 16:49:45 -0500

    wip: boid stubs

---
+++ src/WaterMod/Common/Ambience/_Boids/BoidSystem.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WaterMod.Common.Ambience;
+
+internal struct Boid {
+    public Vector2 Position;
+    public Vector2 Velocity;
+    
+    [JsonIgnore]
+    public Flock ParentFlock;
+    
+    public Color BaseColor;
+    public Color SecondaryColor;
+}
+
+internal struct Flock {
+    public int Id;
+    public List<Boid> Boids;
+}
+
+internal sealed class BoidSystem {
+    private List<Flock> Flocks;
+}
