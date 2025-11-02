f397c28c88daf1621bc7b498d4a8f2bebc0f32b6
mayli4 <sydsleeper@gmail.com>
2025-11-02 16:47:17 -0500

    refactor: make smooth music use daybreak hooking system

--- src/WaterMod/Common/SmoothMusic.cs
+++ src/WaterMod/Common/SmoothMusic.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using Daybreak.Common.Features.Hooks;
+using JetBrains.Annotations;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using NVorbis;
@@ -8,44 +9,42 @@ using Terraria.Audio;
 
 namespace WaterMod.Common;
 
-[UsedImplicitly]
-[Autoload(Side = ModSide.Client)]
-public sealed class SmoothMusic : ILoadable {
+internal sealed class SmoothMusic {
     private static int _previousMusic;
     private static long _previousSamplePosition;
     
-    void ILoadable.Load(Mod mod) {
+    [UsedImplicitly]
+    [OnLoad(Side = ModSide.Client)]
+    static void SubmitPatch() {
         // This patch makes music tracks play subsequently, providing smooth transition between different songs.
-        IL_OGGAudioTrack.PrepareBufferToSubmit += PrepareBufferToSubmitPatch;
-    }
+        IL_OGGAudioTrack.PrepareBufferToSubmit += il =>
+        {
+            try {
+                var c = new ILCursor(il);
 
-    void ILoadable.Unload() { }
-    
-    private static void PrepareBufferToSubmitPatch(ILContext il) {
-        try {
-            var c = new ILCursor(il);
-
-            if (!c.TryGotoNext(i => i.MatchCallOrCallvirt(typeof(OGGAudioTrack).GetMethod("ApplyTemporaryBufferTo", BindingFlags.NonPublic | BindingFlags.Static) ?? throw new InvalidOperationException()))) {
-                return;
-            }
-
-            c.Index--;
-            
-            c.Emit(OpCodes.Ldarg, 0);
-            c.Emit(OpCodes.Ldfld, typeof(OGGAudioTrack).GetField("_vorbisReader", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException());
-            
-            c.EmitDelegate((VorbisReader reader) => {
-
-                if (Main.curMusic != _previousMusic && _previousSamplePosition < reader.TotalSamples) {
-                    reader.SamplePosition = _previousSamplePosition;
+                if (!c.TryGotoNext(i => i.MatchCallOrCallvirt(typeof(OGGAudioTrack).GetMethod("ApplyTemporaryBufferTo", BindingFlags.NonPublic | BindingFlags.Static) ?? throw new InvalidOperationException()))) {
+                    return;
                 }
+
+                c.Index--;
+            
+                c.Emit(OpCodes.Ldarg, 0);
+                c.Emit(OpCodes.Ldfld, typeof(OGGAudioTrack).GetField("_vorbisReader", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException());
+            
+                c.EmitDelegate((VorbisReader reader) => {
+
+                    if (Main.curMusic != _previousMusic && _previousSamplePosition < reader.TotalSamples) {
+                        reader.SamplePosition = _previousSamplePosition;
+                    }
     
-                _previousMusic = Main.curMusic;
-                _previousSamplePosition = reader.SamplePosition;
-            });
-        }
-        catch (Exception) {
-            MonoModHooks.DumpIL(ModContent.GetInstance<ModImpl>(), il);
-        }
+                    _previousMusic = Main.curMusic;
+                    _previousSamplePosition = reader.SamplePosition;
+                });
+            }
+            catch (Exception) {
+                MonoModHooks.DumpIL(ModContent.GetInstance<ModImpl>(), il);
+            }
+        };
     }
+    
 }
