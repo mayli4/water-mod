6d8bf6b7a8655909b1b3d4a49b7a584df696e189
mayli4 <sydsleeper@gmail.com>
2025-11-08 11:56:04 -0500

    feat: common reefs noise generator

---
+++ src/WaterMod/Content/Reefs/_Generation/ReefsGenCommon.cs
@@ -0,0 +1,16 @@
+﻿using Daybreak.Common.Features.Hooks;
+using JetBrains.Annotations;
+using WaterMod.Utilities;
+
+namespace WaterMod.Content.Reefs;
+
+internal class ReefsGenCommon {
+    private static FastNoiseLite _noiseGenerator;
+    
+    [OnLoad]
+    [UsedImplicitly]
+    static void Load() {
+        _noiseGenerator = new FastNoiseLite();
+        _noiseGenerator.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
+    }
+}
