e57303ac3590970b8cf42c42fd3375df6a960feb
itsBuggingMe <lohj002@gmail.com>
2025-11-10 15:53:35 -0500

    replace extension syntax

--- src/WaterMod/Utilities/_Extensions/PacketExtensions.cs
+++ src/WaterMod/Utilities/_Extensions/PacketExtensions.cs
@@ -4,6 +4,7 @@ using WaterMod.Common.Networking;
 namespace WaterMod.Utilities;
 
 internal static class PacketExtensions {
+    /* uncomment when C# 14 is supported in more places
     extension<T>(T inst) where T : unmanaged, IPacket {
         public void Write(BinaryWriter writer) {
             Packet<T>.Write(writer, in inst);
@@ -16,5 +17,13 @@ internal static class PacketExtensions {
         public void Send(int toClient = -1, int ignoreClient = -1) {
             NetworkingHandler.SendPacket(in inst, toClient, ignoreClient);
         }
+    }*/
+
+    public static void Write<T>(this T inst, BinaryWriter writer) where T : unmanaged, IPacket {
+        Packet<T>.Write(writer, in inst);
+    }
+
+    public static void Send<T>(this T inst, int toClient = -1, int ignoreClient = -1) where T : unmanaged, IPacket {
+        NetworkingHandler.SendPacket(in inst, toClient, ignoreClient);
     }
 }
