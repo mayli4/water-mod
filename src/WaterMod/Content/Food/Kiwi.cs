bbbedcb6265354398b3c445787d4a9125dc96e9b
mayli4 <sydsleeper@gmail.com>
2025-12-09 18:21:04 -0500

    feat: Kiwi

---
+++ src/WaterMod/Content/Food/Kiwi.cs
@@ -0,0 +1,34 @@
+﻿using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace WaterMod.Content.Food;
+
+internal sealed class Kiwi : ModItem {
+    public override string Texture => Assets.Textures.Content.Fruits.Kiwi.KEY;
+    
+    public override void SetStaticDefaults() {
+        Item.ResearchUnlockCount = 5;
+
+        ItemID.Sets.IsFood[Type] = true;
+        Main.RegisterItemAnimation(Type, new DrawAnimationVertical(2, 3) { NotActuallyAnimating = true });
+
+        ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.Ambrosia;
+    }
+    
+    public override void SetDefaults() {
+        Item.width = 26;
+        Item.height = 32;
+        Item.rare = ItemRarityID.Blue;
+        Item.maxStack = Item.CommonMaxStack;
+        Item.value = Item.sellPrice(0, 0, 5, 0);
+        Item.noUseGraphic = false;
+        Item.useStyle = ItemUseStyleID.EatFood;
+        Item.useTime = Item.useAnimation = 20;
+        Item.noMelee = true;
+        Item.consumable = true;
+        Item.autoReuse = false;
+        Item.UseSound = SoundID.Item2;
+        Item.buffTime = 5 * 60 * 60;
+        Item.buffType = BuffID.WellFed;
+    }
+}
