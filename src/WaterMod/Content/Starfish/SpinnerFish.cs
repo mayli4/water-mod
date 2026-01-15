78ea0649030352cbbd62e2b8a5d9be9c52906057
mayli4 <sydsleeper@gmail.com>
2026-01-15 02:17:55 -0500

    feat: spinner fish

---
+++ src/WaterMod/Content/Starfish/SpinnerFish.cs
@@ -0,0 +1,209 @@
+﻿using System.IO;
+using Terraria.Enums;
+using Terraria.ID;
+
+namespace WaterMod.Content;
+
+public class SpinnerFishItem : ModItem {
+    public override string Texture => Assets.Images.Content.Starfish.SpinnerFishItem.KEY; 
+    
+    public override void SetDefaults() {
+        base.SetDefaults();
+
+        Item.consumable = true;
+        Item.noMelee = true;
+        Item.channel = true;
+        Item.noUseGraphic = true;
+
+        Item.maxStack = Item.CommonMaxStack;
+
+        Item.DamageType = DamageClass.Melee;
+        Item.SetWeaponValues(9, 1f);
+
+        Item.width = 26;
+        Item.height = 26;
+
+        Item.shoot = ModContent.ProjectileType<SpinnerFishProjectile>();
+        Item.shootSpeed = 10f;
+
+        Item.useTime = 20;
+        Item.useAnimation = 20;
+        Item.UseSound = SoundID.Item1;
+        Item.useStyle = ItemUseStyleID.Swing;
+
+        Item.SetShopValues(ItemRarityColor.Blue1, Item.buyPrice());
+    }
+
+    public override void AddRecipes() {
+        base.AddRecipes();
+
+        CreateRecipe(50)
+            .AddIngredient<EnchantedSandMaterial>()
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
+
+public class SpinnerFishProjectile : ModProjectile {
+    public override string Texture => Assets.Images.Content.Starfish.SpinnerFishProjectile.KEY; 
+    
+    public ref float Target => ref Projectile.ai[0];
+    public ref float Timer => ref Projectile.ai[1];
+
+    public bool StickingToNpc { get; private set; }
+    public bool StickingToTile { get; private set; }
+
+    public bool StickingToAnything => StickingToNpc || StickingToTile;
+
+    private Vector2 offset;
+
+    public override void SetDefaults() {
+        base.SetDefaults();
+
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.friendly = true;
+
+        Projectile.width = 16;
+        Projectile.height = 16;
+
+        Projectile.aiStyle = -1;
+
+        Projectile.timeLeft = 180;
+        Projectile.penetrate = -1;
+        Projectile.localNPCHitCooldown = 30;
+    }
+
+    public override void SendExtraAI(BinaryWriter writer) {
+        base.SendExtraAI(writer);
+
+        writer.Write(StickingToNpc);
+        writer.Write(StickingToTile);
+    }
+
+    public override void ReceiveExtraAI(BinaryReader reader) {
+        base.ReceiveExtraAI(reader);
+
+        StickingToNpc = reader.ReadBoolean();
+        StickingToTile = reader.ReadBoolean();
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        base.OnHitNPC(target, hit, damageDone);
+
+        Projectile.scale = 1.25f;
+        Projectile.rotation += MathHelper.ToRadians(Main.rand.NextFloat(5f, 15f));
+
+        if (StickingToAnything) {
+            return;
+        }
+
+        offset = target.Center - Projectile.Center + Projectile.velocity;
+
+        Target = target.whoAmI;
+        StickingToNpc = true;
+
+        NetMessage.SendData(MessageID.SyncProjectile, -1, -1, null, Projectile.whoAmI);
+    }
+
+    public override bool OnTileCollide(Vector2 oldVelocity) {
+        if (StickingToAnything) {
+            return false;
+        }
+
+        Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+
+        StickingToTile = true;
+
+        NetMessage.SendData(MessageID.SyncProjectile, -1, -1, null, Projectile.whoAmI);
+
+        return false;
+    }
+
+    public override void AI() {
+        base.AI();
+
+        Projectile.scale = MathHelper.Lerp(Projectile.scale, 1f, 0.2f);
+
+        if (Projectile.timeLeft < 255 / 25) {
+            Projectile.alpha += 25;
+        }
+
+        UpdateTargetStick();
+        UpdateTileStick();
+
+        if (StickingToAnything) {
+            return;
+        }
+
+        UpdateGravity();
+
+        Projectile.rotation += Projectile.velocity.X * 0.1f;
+    }
+
+    // TODO: Implement proper visuals.
+    public override bool PreDraw(ref Color lightColor) {
+        var texture = ModContent.Request<Texture2D>(Texture + "_Outline").Value;
+        var effects = Projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+        var offsetX = 0;
+        var offsetY = 0;
+        var originX = (texture.Width - Projectile.width) / 2f + Projectile.width / 2f;
+
+        ProjectileLoader.DrawOffset(Projectile, ref offsetX, ref offsetY, ref originX);
+
+        var x = Projectile.position.X - Main.screenPosition.X + originX + offsetX;
+        var y = Projectile.position.Y - Main.screenPosition.Y + Projectile.height / 2f + Projectile.gfxOffY;
+
+        var frame = texture.Frame(1, Main.projFrames[Projectile.type], frameY: Projectile.frame);
+        var origin = new Vector2(originX, Projectile.height / 2f + offsetY);
+
+        Main.EntitySpriteDraw(
+            texture,
+            new Vector2(x, y),
+            frame,
+            Projectile.GetAlpha(Color.White),
+            Projectile.rotation,
+            origin,
+            Projectile.scale,
+            effects
+        );
+
+        return true;
+    }
+
+    private void UpdateTargetStick() {
+        if (!StickingToNpc) {
+            return;
+        }
+
+        var target = Main.npc[(int)Target];
+
+        if (!target.active) {
+            Projectile.Kill();
+            return;
+        }
+
+        Projectile.tileCollide = false;
+
+        Projectile.Center = target.Center - offset;
+        Projectile.gfxOffY = target.gfxOffY;
+    }
+
+    private void UpdateTileStick() {
+        if (!StickingToTile) {
+            return;
+        }
+
+        Projectile.velocity *= 0.5f;
+    }
+
+    private void UpdateGravity() {
+        Timer++;
+
+        if (Timer < 10f) {
+            return;
+        }
+
+        Projectile.velocity.Y += 0.2f;
+    }
+}
